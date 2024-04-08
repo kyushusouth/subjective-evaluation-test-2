6016f77b0aed58448ec16d21b8d1d3e6060f7701
Taisuke Minami <tmnm13009@gmail.com>
2024-04-08 12:30:12 +0900

    api実装

--- app/api/sampleMetaData/route.ts
+++ app/api/sampleMetaData/route.ts
@@ -1,8 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/prefer-default-export */
-import { fetchAllwavFiles } from "@/app/lib/data";
+import { prisma } from "@/app/lib/prisma";
+
+const shuffleArray = (array: object[]) => {
+  const cloneArray = [...array];
+  for (let i = cloneArray.length - 1; i >= 0; i -= 1) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    const tmpStorage = cloneArray[i];
+    cloneArray[i] = cloneArray[rand];
+    cloneArray[rand] = tmpStorage;
+  }
+  return cloneArray;
+};
 
 export async function GET(request: Request) {
-  const sampleMetaDataList = await fetchAllwavFiles();
-  return Response.json(sampleMetaDataList);
+  try {
+    const sampleMetaDataList = await prisma.sampleMetaData.findMany({
+      take: 30,
+    });
+    const sampleMetaDataListShuffled = shuffleArray(sampleMetaDataList);
+    return Response.json(sampleMetaDataListShuffled);
+  } catch (error) {
+    throw new Error("Failed to fetch all wav files.");
+  }
 }
