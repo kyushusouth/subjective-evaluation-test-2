11731da7085d5efef7e598cdbde0d75b73d7b0f1
Taisuke Minami <tmnm13009@gmail.com>
2024-04-10 12:10:54 +0900

    evalページで最初にデータを取得するサーバーコンポーネントの実装

--- app/eval/page.tsx
+++ app/eval/page.tsx
@@ -1,169 +1,37 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { useForm, FormProvider } from "react-hook-form";
-import EvalForm from "@/app/eval/form";
-import Confirm from "@/app/eval/confirm";
-import { SchemaType } from "@/app/eval/schema";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import Contents from "@/app/eval/Contents";
 import {
-  SampleMetaData,
-  NaturalnessItem,
-  IntelligibilityItem,
-  Respondents,
-} from "@prisma/client";
+  fetchSampleMetaDataListShuffled,
+  fetchNaturalnessItemList,
+  fetchIntelligibilityList,
+  fetchRespondent,
+} from "@/app/lib/data";
 
-export default function Page() {
-  const methods = useForm<SchemaType>({
-    mode: "onBlur",
-  });
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [sampleMetaDataList, setSampleMetaDataList] = useState<
-    SampleMetaData[]
-  >([]);
-  const [domainName, setDomainName] = useState<string>("");
-  const [bucketName, setBucketName] = useState<string>("");
-  const [naturalnessItemList, setNaturalnessItemList] = useState<
-    NaturalnessItem[]
-  >([]);
-  const [intelligibilityItemList, setintelligibilityItemList] = useState<
-    IntelligibilityItem[]
-  >([]);
-  const [respondent, setRespondent] = useState<Respondents | undefined>();
-  const numSamplePerPage = 10;
-  const lastPageNumber = Math.ceil(
-    sampleMetaDataList.length / numSamplePerPage,
-  );
+export default async function Page() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user === null) {
+    redirect("/login");
+  }
 
-  const onNext = () => {
-    setPageNumber((state) => state + 1);
-  };
-
-  const onPrev = () => {
-    setPageNumber((state) => state - 1);
-  };
-
-  useEffect(() => {
-    let ignore = false;
-    const fetchSampleMetaData = async () => {
-      const res = await fetch("api/sampleMetaData", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setSampleMetaDataList(data);
-      }
-    };
-
-    const fetchDomainName = async () => {
-      const res = await fetch("api/domainName", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setDomainName(data);
-      }
-    };
-
-    const fetchBucketName = async () => {
-      const res = await fetch("api/bucketName", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setBucketName(data);
-      }
-    };
-
-    const fetchNaturalnessItem = async () => {
-      const res = await fetch("api/naturalnessItem", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setNaturalnessItemList(data);
-      }
-    };
-
-    const fetchIntelligibilityItem = async () => {
-      const res = await fetch("api/intelligibilityItem", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setintelligibilityItemList(data);
-      }
-    };
-
-    const fetchRespondent = async () => {
-      const res = await fetch("api/respondent", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "GET",
-      });
-      const data = await res.json();
-      if (!ignore) {
-        setRespondent(data);
-      }
-    };
-
-    fetchSampleMetaData();
-    fetchDomainName();
-    fetchBucketName();
-    fetchNaturalnessItem();
-    fetchIntelligibilityItem();
-    fetchRespondent();
-
-    return () => {
-      ignore = true;
-    };
-  }, []);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  });
+  const sampleMetaDataListShuffled = await fetchSampleMetaDataListShuffled();
+  const naturalnessItemList = await fetchNaturalnessItemList();
+  const intelligibilityItemList = await fetchIntelligibilityList();
+  const respondent = await fetchRespondent();
+  const domainName = process.env.GCS_DOMAIN_NAME;
+  const bucketName = process.env.GCS_BUCKET_NAME;
 
   return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <FormProvider {...methods}>
-      <form>
-        {pageNumber === lastPageNumber + 1 ? (
-          <Confirm
-            onPrev={onPrev}
-            sampleMetaDataList={sampleMetaDataList}
-            respondent={respondent}
-          />
-        ) : (
-          <EvalForm
-            onNext={onNext}
-            onPrev={onPrev}
-            sampleMetaDataList={sampleMetaDataList.slice(
-              numSamplePerPage * (pageNumber - 1),
-              numSamplePerPage * pageNumber,
-            )}
-            domainName={domainName}
-            bucketName={bucketName}
-            naturalnessItemList={naturalnessItemList}
-            intelligibilityItemList={intelligibilityItemList}
-            pageNumber={pageNumber}
-          />
-        )}
-      </form>
-    </FormProvider>
+    <Contents
+      sampleMetaDataList={sampleMetaDataListShuffled}
+      domainName={domainName}
+      bucketName={bucketName}
+      naturalnessItemList={naturalnessItemList}
+      intelligibilityItemList={intelligibilityItemList}
+      respondent={respondent}
+    />
   );
 }
