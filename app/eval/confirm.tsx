4c4f9695092b7447ec687929766723fe5bd2fe89
Taisuke Minami <tmnm13009@gmail.com>
2024-04-10 12:09:08 +0900

    提出中UIの実装

--- app/eval/confirm.tsx
+++ app/eval/confirm.tsx
@@ -2,75 +2,14 @@
 
 /* eslint-disable no-restricted-syntax */
 import { useFormContext } from "react-hook-form";
-import { useState, useEffect } from "react";
+import clsx from "clsx";
 import { SchemaType } from "@/app/eval/schema";
-import { SampleMetaData, Respondents } from "@prisma/client";
-import { useRouter } from "next/navigation";
 
-export default function Confirm({
-  onPrev,
-  sampleMetaDataList,
-  respondent,
-}: {
-  onPrev: () => void;
-  sampleMetaDataList: SampleMetaData[];
-  respondent: Respondents | undefined;
-}) {
-  const router = useRouter();
+export default function Confirm({ onPrev }: { onPrev: () => void }) {
   const methods = useFormContext<SchemaType>();
-  const { handleSubmit } = methods;
-  const [sendData, setSendData] = useState<SchemaType | null>(null);
-
-  function onSubmit(data: SchemaType) {
-    setSendData(data);
-  }
-
-  function onError(error: SchemaType) {
-    console.log("error", error);
-  }
-
-  useEffect(() => {
-    handleSubmit(onSubmit, onError)();
-  }, []);
-
-  const handleClick = () => {
-    const fetchDataList = async (dataList: object[]) => {
-      const response = await fetch("api/answers", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dataList),
-        method: "POST",
-      });
-      const result = await response.json();
-      if (result.success) {
-        router.push("/");
-      } else {
-        router.push("/eval");
-      }
-    };
-
-    if (sendData === null) {
-      throw new Error("sendData is null.");
-    }
-    if (respondent === undefined) {
-      throw new Error("respondent is undefined.");
-    }
-
-    const dataList = [];
-    for (const sampleMetaData of sampleMetaDataList) {
-      const sampleId = Number(sampleMetaData.id);
-      const naturalness = Number(sendData[`naturalness_${sampleId}`]);
-      const intelligibility = Number(sendData[`intelligibility_${sampleId}`]);
-      dataList.push({
-        respondent_id: respondent.id,
-        sample_meta_data_id: sampleId,
-        naturalness_id: naturalness,
-        intelligibility_id: intelligibility,
-      });
-    }
-    fetchDataList(dataList);
-  };
+  const {
+    formState: { isValid, isSubmitting, isSubmitted },
+  } = methods;
 
   return (
     <div className="flex flex-row justify-center items-center gap-10 my-10">
@@ -81,13 +20,44 @@ export default function Confirm({
       >
         戻る
       </button>
-      <button
-        type="button"
-        className="bg-slate-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-        onClick={handleClick}
-      >
-        提出する
-      </button>
+
+      {isSubmitting || isSubmitted ? (
+        <button
+          disabled
+          type="button"
+          className="bg-blue-700 text-white py-2 px-4 rounded"
+        >
+          <svg
+            aria-hidden="true"
+            role="status"
+            className="inline w-4 h-4 me-3 text-white animate-spin"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="#E5E7EB"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentColor"
+            />
+          </svg>
+          処理中...
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className={clsx("bg-slate-500 text-white py-2 px-4 rounded", {
+            "hover:bg-blue-700": isValid,
+            "cursor-not-allowed bg-slate-500/50": !isValid,
+          })}
+          disabled={!isValid}
+        >
+          提出する
+        </button>
+      )}
     </div>
   );
 }
