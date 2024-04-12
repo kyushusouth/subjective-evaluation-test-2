49c418b58a175087cb7a3db837eeff6661c61b11
Taisuke Minami <tmnm13009@gmail.com>
2024-04-12 11:47:00 +0900

    年齢性別回答フォーム用のPOST関数追加

--- app/api/respondent/route.ts
+++ app/api/respondent/route.ts
@@ -23,3 +23,47 @@ export async function GET(request: Request) {
     throw new Error("Failed to fetch naturalness item.");
   }
 }
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  const supabase = createClient();
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user === null) {
+      redirect("/login");
+    }
+    await prisma.respondents.update({
+      where: {
+        auth_id: user?.id,
+      },
+      data: {
+        age: Number(data.age),
+        sex: String(data.sex),
+      },
+    });
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "Respondent Information was updated successfully.",
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ success: false, message: "Failed to create Answers." }),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+}
