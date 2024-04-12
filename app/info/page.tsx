5aa4157e7dee9abc6b55e584c86e0988681f668b
Taisuke Minami <tmnm13009@gmail.com>
2024-04-12 11:47:47 +0900

    react hook formを利用した実装に変更

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,10 +1,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import createInfo from "@/app/info/actions";
 import { fetchSexItemList, fetchRespondent } from "@/app/lib/data";
-import SubmitButton from "@/app/info/SubmitButton";
+import Contents from "@/app/info/Contents";
 
-export default async function Info() {
+export default async function Page() {
   const supabase = createClient();
   const {
     data: { user },
@@ -16,41 +15,5 @@ export default async function Info() {
   const sexItemList = await fetchSexItemList();
   const respondent = await fetchRespondent();
 
-  return (
-    <div className="my-10">
-      <form action={createInfo}>
-        <div className="flex flex-col justify-center items-center gap-10">
-          <label htmlFor="age" className="w-full">
-            年齢
-            <input
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-50 focus:bg-white"
-              id="age"
-              name="age"
-              type="number"
-              defaultValue={
-                respondent?.age === -1 ? undefined : respondent?.age
-              }
-              required
-            />
-          </label>
-          <label htmlFor="sex" className="w-full">
-            性別
-            <select
-              id="sex"
-              name="sex"
-              className="w-full px-3 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-gray-50 focus:bg-white"
-              defaultValue={respondent?.sex}
-            >
-              {sexItemList.map((sexItem) => (
-                <option key={sexItem.id} value={sexItem.item}>
-                  {sexItem.item}
-                </option>
-              ))}
-            </select>
-          </label>
-          <SubmitButton>提出する</SubmitButton>
-        </div>
-      </form>
-    </div>
-  );
+  return <Contents sexItemList={sexItemList} respondent={respondent!} />;
 }
