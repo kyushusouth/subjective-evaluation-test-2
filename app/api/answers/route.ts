ae7d47d091bb93630ca4b114c4124f5bebc93edf
Taisuke Minami <tmnm13009@gmail.com>
2024-04-13 09:48:18 +0900

    回答を終了した否かを表す値のupdateを追加

--- app/api/answers/route.ts
+++ app/api/answers/route.ts
@@ -1,13 +1,48 @@
 /* eslint-disable import/prefer-default-export */
+import { createClient } from "@/utils/supabase/server";
 import { prisma } from "@/app/lib/prisma";
+import { redirect } from "next/navigation";
 
 export async function POST(request: Request) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user === null) {
+    redirect("/login");
+  }
+
   const answersData = await request.json();
+  const fromUrl = request.headers.get("referer")!;
+  const fromUrlSplit = fromUrl.split("/");
+  const pageName = fromUrlSplit[fromUrlSplit.length - 1];
+
   try {
     await prisma.answers.createMany({
       data: answersData,
       skipDuplicates: true,
     });
+
+    if (pageName === "eval_practice") {
+      await prisma.respondents.update({
+        where: {
+          auth_id: user.id,
+        },
+        data: {
+          is_finished_practice: true,
+        },
+      });
+    } else if (pageName === "eval") {
+      await prisma.respondents.update({
+        where: {
+          auth_id: user.id,
+        },
+        data: {
+          is_finished_eval: true,
+        },
+      });
+    }
+
     return new Response(
       JSON.stringify({
         success: true,
