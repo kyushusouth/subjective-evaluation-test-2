99cf7a3a6b52675794681cde6a473aafdfbf1214
Taisuke Minami <tmnm13009@gmail.com>
2024-04-13 10:21:11 +0900

    delete

--- app/lib/env.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({ path: "../../.env" });
-export const domainName = process.env.GCS_DOMAIN_NAME;
-export const bucketName = process.env.GCS_BUCKET_NAME;
