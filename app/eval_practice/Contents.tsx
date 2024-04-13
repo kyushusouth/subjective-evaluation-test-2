704abde61c8e5d075e94cc61612d70e266d6f0c6
Taisuke Minami <tmnm13009@gmail.com>
2024-04-13 09:48:58 +0900

    練習試行追加

---
+++ app/eval_practice/Contents.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import {
+  SampleMetaData,
+  NaturalnessItem,
+  IntelligibilityItem,
+  Respondents,
+} from "@prisma/client";
+import Form from "@/app/eval/Form";
+import Confirm from "@/app/eval/Confirm";
+import { SchemaType } from "@/app/eval/schema";
+
+export default function Content({
+  sampleMetaDataList,
+  domainName,
+  bucketName,
+  naturalnessItemList,
+  intelligibilityItemList,
+  respondent,
+}: {
+  sampleMetaDataList: SampleMetaData[];
+  domainName: string;
+  bucketName: string;
+  naturalnessItemList: NaturalnessItem[];
+  intelligibilityItemList: IntelligibilityItem[];
+  respondent: Respondents | undefined;
+}) {
+  const methods = useForm<SchemaType>({
+    mode: "onSubmit",
+  });
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const numSamplePerPage = 10;
+  const lastPageNumber = Math.ceil(
+    sampleMetaDataList.length / numSamplePerPage,
+  );
+  const router = useRouter();
+  const { handleSubmit } = methods;
+
+  const onNext = () => {
+    setPageNumber((state) => state + 1);
+    window.scrollTo(0, 0);
+  };
+
+  const onPrev = () => {
+    setPageNumber((state) => state - 1);
+    window.scrollTo(0, 0);
+  };
+
+  const onSubmit = async (data: SchemaType) => {
+    const dataList = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const sampleMetaData of sampleMetaDataList) {
+      const sampleId = Number(sampleMetaData.id);
+      const naturalness = Number(data[`naturalness_${sampleId}`]);
+      const intelligibility = Number(data[`intelligibility_${sampleId}`]);
+      dataList.push({
+        respondent_id: respondent?.id,
+        sample_meta_data_id: sampleId,
+        naturalness_id: naturalness,
+        intelligibility_id: intelligibility,
+      });
+    }
+    const response = await fetch("api/answers", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataList),
+      method: "POST",
+    });
+    const result = await response.json();
+    if (result.success) {
+      router.push("/thanks");
+    } else {
+      router.push("/eval");
+    }
+  };
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {pageNumber === lastPageNumber + 1 ? (
+          <Confirm onPrev={onPrev} />
+        ) : (
+          <Form
+            onNext={onNext}
+            onPrev={onPrev}
+            sampleMetaDataList={sampleMetaDataList.slice(
+              numSamplePerPage * (pageNumber - 1),
+              numSamplePerPage * pageNumber,
+            )}
+            domainName={domainName}
+            bucketName={bucketName}
+            naturalnessItemList={naturalnessItemList}
+            intelligibilityItemList={intelligibilityItemList}
+            pageNumber={pageNumber}
+          />
+        )}
+      </form>
+    </FormProvider>
+  );
+}
