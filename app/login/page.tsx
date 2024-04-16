42d2599577676745cd09958d918086f3fdefb659
Taisuke Minami <tmnm13009@gmail.com>
2024-04-17 08:37:33 +0900

    エラーメッセージのUI変更

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -35,7 +35,7 @@ export default async function Login({
           {searchParams?.message && (
             <div className="text-center">
               <div
-                className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-400"
+                className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
                 role="alert"
               >
                 認証に失敗しました。
