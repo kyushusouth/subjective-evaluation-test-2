8c63e690616b6d7fef892c04f2879afd5491f32b
Taisuke Minami <tmnm13009@gmail.com>
2024-04-17 08:37:58 +0900

    debug

--- app/NavBarDrawer.tsx
+++ app/NavBarDrawer.tsx
@@ -39,9 +39,9 @@ function Drawer({ respondent }: { respondent: Respondents }) {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M1 1h15M1 7h15M1 13h15"
           />
         </svg>
@@ -87,9 +87,9 @@ function Drawer({ respondent }: { respondent: Respondents }) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
               />
             </svg>
