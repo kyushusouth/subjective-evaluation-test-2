7873cf428d074ac1430ced317ccf8e49aa4aea7f
Taisuke Minami <tmnm13009@gmail.com>
2024-04-17 10:40:54 +0900

    update

--- app/api/respondent/route.ts
+++ app/api/respondent/route.ts
@@ -2,19 +2,14 @@
 /* eslint-disable import/prefer-default-export */
 import { prisma } from "@/app/lib/prisma";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
 export async function POST(request: Request) {
   const data = await request.json();
   const supabase = createClient();
   try {
-    throw new Error();
     const {
       data: { user },
     } = await supabase.auth.getUser();
-    if (user === null) {
-      redirect("/login");
-    }
     await prisma.respondents.update({
       where: {
         auth_id: user?.id,
@@ -25,30 +20,18 @@ export async function POST(request: Request) {
         is_finished_info: true,
       },
     });
-    return new Response(
-      JSON.stringify({
-        success: true,
-        message: "Respondent Information was updated successfully.",
-      }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+    });
   } catch (error) {
-    return new Response(
-      JSON.stringify({
-        success: false,
-        message: "Failed to update Respondents.",
-      }),
-      {
-        status: 400,
-        headers: {
-          "Content-Type": "application/json",
-        },
+    return new Response(JSON.stringify({ success: false }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+    });
   }
 }
