a37e2c67192ce7c9f09f7153c32c2526867f3b96
Taisuke Minami <tmnm13009@gmail.com>
2024-04-17 10:41:06 +0900

    想定外のエラーが起きた時のページを実装

---
+++ app/error/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="text-center">
+      <div
+        className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
+        role="alert"
+      >
+        想定外のエラーが発生しました。
+      </div>
+    </div>
+  );
+}
