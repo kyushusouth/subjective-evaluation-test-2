5616bce6f81d99c2bb2bc24bd894c9a865a81dcd
Taisuke Minami <tmnm13009@gmail.com>
2024-06-03 01:14:54 +0900

    試行のトップページを説明とし、その次のページから実験を開始するように変更

---
+++ app/eval_practice/exp/page.tsx
@@ -0,0 +1,42 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import Contents from "@/app/eval/Contents";
+import {
+  fetchSampleMetaDataListShuffled,
+  fetchNaturalnessItemList,
+  fetchIntelligibilityList,
+  fetchRespondent,
+} from "@/app/lib/data";
+
+export default async function Page() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user === null) {
+    redirect("/login");
+  }
+
+  const numSamplePerPage = 5;
+  const sampleMetaDataListShuffled = await fetchSampleMetaDataListShuffled(
+    undefined,
+    "eval_practice",
+  );
+  const naturalnessItemList = await fetchNaturalnessItemList();
+  const intelligibilityItemList = await fetchIntelligibilityList();
+  const respondent = await fetchRespondent();
+  const domainName = process.env.GCS_DOMAIN_NAME;
+  const bucketName = process.env.GCS_BUCKET_NAME;
+
+  return (
+    <Contents
+      sampleMetaDataList={sampleMetaDataListShuffled}
+      domainName={domainName!}
+      bucketName={bucketName!}
+      naturalnessItemList={naturalnessItemList}
+      intelligibilityItemList={intelligibilityItemList}
+      respondent={respondent!}
+      numSamplePerPage={numSamplePerPage}
+    />
+  );
+}
--- app/eval_practice/page.tsx
+++ app/eval_practice/page.tsx
@@ -1,12 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import Contents from "@/app/eval/Contents";
-import {
-  fetchSampleMetaDataListShuffled,
-  fetchNaturalnessItemList,
-  fetchIntelligibilityList,
-  fetchRespondent,
-} from "@/app/lib/data";
+import Link from "next/link";
 
 export default async function Page() {
   const supabase = createClient();
@@ -17,26 +11,91 @@ export default async function Page() {
     redirect("/login");
   }
 
-  const numSamplePerPage = 5;
-  const sampleMetaDataListShuffled = await fetchSampleMetaDataListShuffled(
-    undefined,
-    "eval_practice",
-  );
-  const naturalnessItemList = await fetchNaturalnessItemList();
-  const intelligibilityItemList = await fetchIntelligibilityList();
-  const respondent = await fetchRespondent();
-  const domainName = process.env.GCS_DOMAIN_NAME;
-  const bucketName = process.env.GCS_BUCKET_NAME;
-
   return (
-    <Contents
-      sampleMetaDataList={sampleMetaDataListShuffled}
-      domainName={domainName!}
-      bucketName={bucketName!}
-      naturalnessItemList={naturalnessItemList}
-      intelligibilityItemList={intelligibilityItemList}
-      respondent={respondent!}
-      numSamplePerPage={numSamplePerPage}
-    />
+    <div className="flex flex-col gap-10">
+      <section className="space-y-2 text-base">
+        <h1 className="text-xl text-center mb-8">練習試行について</h1>
+        <p className="leading-relaxed">
+          練習試行は、本番試行の前に行っていただく練習のための実験となります。
+          <br />
+          こちらは何度行っていただいても構いませんので、実験内容の把握にご利用ください。
+        </p>
+      </section>
+
+      <hr className="border-t border-gray-300" />
+
+      <section className="space-y-4 text-base">
+        <h2 className="text-lg">実験内容</h2>
+        <p className="leading-relaxed">
+          実験で行っていただくのは、下記二つです。
+        </p>
+        <ol className="leading-relaxed list-decimal list-inside">
+          <li>音声を聞く。</li>
+          <li>音声の自然性・明瞭性を五段階で評価する。</li>
+        </ol>
+        <p className="leading-relaxed">
+          評価にあたり、音声は何度聞いていただいても構いません。
+          <br />
+          評価項目である自然性・明瞭性については、どのような観点で評価するかを以下に記載しております。
+          <br />
+          ご確認ください。
+        </p>
+      </section>
+
+      <hr className="border-t border-gray-300" />
+
+      <section className="space-y-4 text-base">
+        <h2 className="text-lg">自然性とは？</h2>
+        <p className="leading-relaxed">
+          一つ目の評価項目である自然性は、
+          <br />
+          <span className="font-bold">
+            発話内容によらず、その音声がどれくらい人間らしく自然なものに聞こえたか
+          </span>
+          を指します。
+          <br />
+          例えば、音質自体やイントネーションの自然さなどが評価の観点として挙げられます。
+        </p>
+        <p>各音声ごとに、下記の五段階で評価してください。</p>
+        <ol>
+          <li>1: 非常に悪い</li>
+          <li>2: 悪い</li>
+          <li>3: 普通</li>
+          <li>4: 良い</li>
+          <li>5: 非常に良い</li>
+        </ol>
+      </section>
+
+      <hr className="border-t border-gray-300" />
+
+      <section className="space-y-4 text-base">
+        <h2 className="text-lg">明瞭性とは？</h2>
+        <p className="leading-relaxed">
+          二つ目の評価項目である明瞭性は、
+          <br />
+          <span className="font-bold">
+            発話内容自体がどれくらい聞き取りやすかったか
+          </span>
+          を指します。
+          <br />
+          聞き取りやすさのみを評価の観点とする点が、自然性と異なります。
+        </p>
+        <p>各音声ごとに、下記の五段階で評価してください。</p>
+        <ol>
+          <li>1: 非常に悪い</li>
+          <li>2: 悪い</li>
+          <li>3: 普通</li>
+          <li>4: 良い</li>
+          <li>5: 非常に良い</li>
+        </ol>
+      </section>
+
+      <button
+        type="button"
+        className="bg-slate-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-fit mx-auto"
+      >
+        <Link href="/eval_practice/exp">練習試行を開始</Link>
+      </button>
+    </div>
   );
 }
