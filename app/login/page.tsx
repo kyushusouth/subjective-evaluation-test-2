09463bbac8cc0aaa781bff0375cb6c447f21bc34
Taisuke Minami <tmnm13009@gmail.com>
2024-06-03 01:17:10 +0900

    パスワードを表示できるように修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,26 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/jsx-props-no-spreading */
+
+"use client";
+
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 import login from "@/app/login/action";
 import SubmitButton from "@/app/login/SubmitButton";
 
-export default async function Login({
+export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <div className="my-10">
       <form action={login}>
@@ -24,13 +37,22 @@ export default async function Login({
           </label>
           <label htmlFor="password" className="w-full">
             パスワード
-            <input
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-50 focus:bg-white"
-              id="password"
-              name="password"
-              type="password"
-              required
-            />
+            <div className="relative w-full flex items-center">
+              <input
+                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-50 focus:bg-white"
+                id="password"
+                name="password"
+                type={passwordVisible ? "text" : "password"}
+                required
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-3"
+              >
+                <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+              </button>
+            </div>
           </label>
           {searchParams?.message && (
             <div className="text-center">
