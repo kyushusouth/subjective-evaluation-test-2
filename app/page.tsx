c528d020041bac6c68e8fdbdc6f61f27b27a67bf
Taisuke Minami <tmnm13009@gmail.com>
2024-06-03 01:12:09 +0900

    文言変更

--- app/page.tsx
+++ app/page.tsx
@@ -18,74 +18,83 @@ export default async function Index() {
         <p className="leading-relaxed">
           この度はお忙しい中実験にご協力いただき、誠にありがとうございます。
           <br />
-          本実験では、以下の項目を実施していただきます。
-        </p>
-        <ul className="list-disc list-inside leading-relaxed">
-          <li>性別・年齢の回答</li>
-          <li>練習試行</li>
-          <li>本番試行</li>
-          <li>クラウドワークスでの連絡</li>
-        </ul>
-        <p className="leading-relaxed">
           本実験で取得したデータは研究における学会発表や論文執筆のために利用しますが、個人を特定できるような処理は一切致しません。
           <br />
           <br />
           以下、実験項目の説明を記載しておりますので、ご確認の上進めていただきますようよろしくお願い致します。
         </p>
       </section>
+
       <hr className="border-t border-gray-300" />
+
       <section className="space-y-4 text-base">
-        <h2 className="text-lg">性別・年齢の回答</h2>
+        <h2 className="text-lg">メニュー表示</h2>
         <p className="leading-relaxed">
-          性別と性別を回答していただきます。
+          右上のアイコン（三本線のアイコン）をクリックしていただくと、メニューが表示されます。
           <br />
-          右上の<span className="font-bold text-blue-700">メニュー</span>
-          アイコンから、
-          <span className="font-bold text-blue-700">性別・年齢</span>
-          をクリックすることで回答ページにアクセス頂けます。
-          <br />
-          一度提出しますと、回答の修正は不可能となりますのでご注意ください。
+          以下、各項目について説明しておりますので、ご確認ください。
         </p>
       </section>
+
       <hr className="border-t border-gray-300" />
+
+      <section className="space-y-4 text-base">
+        <h2 className="text-lg">アンケートの回答</h2>
+        <p className="leading-relaxed">
+          アンケートでは、性別・年齢など回答者様の基本情報をお答えいただきます。
+          <br />
+          メニューから、
+          <span className="font-bold text-blue-700">アンケート</span>
+          をクリックいだたくことで回答ページにアクセス頂けます。
+          <br />
+          <span className="font-bold">
+            一度提出しますと、回答の修正は不可能となりますのでご注意ください。
+          </span>
+        </p>
+      </section>
+
+      <hr className="border-t border-gray-300" />
+
       <section className="space-y-4 text-base">
         <h2 className="text-lg">練習試行</h2>
         <p className="leading-relaxed">
           練習試行は、本番試行の前に行っていただく練習のための実験となります。
           <br />
-          右上の<span className="font-bold text-blue-700">メニュー</span>
-          アイコンから、
+          メニューから、
           <span className="font-bold text-blue-700">練習試行</span>
-          をクリックすることで回答ページにアクセス頂けます。
+          をクリックいただくことで回答ページにアクセス頂けます。
           <br />
           練習試行は何度行っていただいても構いませんので、こちらを通して実験内容の把握をお願い致します。
         </p>
       </section>
+
       <hr className="border-t border-gray-300" />
+
       <section className="space-y-4 text-base">
         <h2 className="text-lg">本番試行</h2>
         <p className="leading-relaxed">
           本番試行は、練習試行の後に行っていただく本番の実験になります。
           <br />
-          右上の<span className="font-bold text-blue-700">メニュー</span>
-          アイコンから、
+          メニューから、
           <span className="font-bold text-blue-700">本番試行</span>
-          をクリックすることで回答ページにアクセス頂けます。
+          をクリックいただくことで回答ページにアクセス頂けます。
           <br />
           練習試行を一度も行っていない場合、本番試行を選択できなくなっていると思いますが、そのような仕様のため問題ありません。
           <br />
           練習試行を一度完了しますと、選択可能になります。
           <br />
-          一度提出しますと、回答の修正は不可能となりますのでご注意ください。
+          <span className="font-bold">
+            一度提出しますと、回答の修正は不可能となりますのでご注意ください。
+          </span>
         </p>
       </section>
+
       <hr className="border-t border-gray-300" />
+
       <section className="space-y-4 text-base">
-        <h2 className="text-lg">報告</h2>
+        <h2 className="text-lg">実験終了の報告</h2>
         <p className="leading-relaxed">
           全ての実験項目を完了次第、クラウドワークスにて連絡をお願いします。
-          <br />
-          回答を確認次第、承認させていただきます。
         </p>
       </section>
     </div>
