f467066eade028675967451d3fd06bad66ff0f0d
Taisuke Minami <tmnm13009@gmail.com>
2024-06-08 00:57:46 +0900

    update

--- tests/test_1.spec.ts
+++ tests/test_1.spec.ts
@@ -25,10 +25,10 @@ async function reset(respondentId: number) {
   });
 }
 
-const respondentId = 1;
+const respondentId = 2;
 const email = `user${respondentId - 1}@test.com`;
 const wrongEmail = "wrong@test.com";
-const password = `HSE1Gh`;
+const password = `xEJmbF`;
 const wrongPassword = "wrong";
 const age = String(generateRandomInteger(1, 100));
 const sexItemList = ["男性", "女性", "無回答"];
