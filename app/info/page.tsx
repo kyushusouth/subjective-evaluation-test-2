1fc7b50c65f91cc98eac0f7c371bd3ce23619d20
Taisuke Minami <tmnm13009@gmail.com>
2024-06-15 20:11:36 +0900

    is_finished_infoがtrueになった時のリダイレクトにおいて、try-catchでエラーが発生するため修正

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -7,26 +7,20 @@ import {
 import Contents from "@/app/info/Contents";
 
 export default async function Page() {
-  try {
-    const sexItemList = await fetchSexItemList();
-    const audioDeviceItemList = await fetchAudioDeviceItemList();
-    const respondent = await fetchRespondent();
+  const sexItemList = await fetchSexItemList();
+  const audioDeviceItemList = await fetchAudioDeviceItemList();
+  const respondent = await fetchRespondent();
 
-    if (respondent?.is_finished_info) {
-      redirect("/");
-      return null;
-    }
-
-    return (
-      <Contents
-        sexItemList={sexItemList}
-        audioDeviceItemList={audioDeviceItemList}
-        respondent={respondent!}
-      />
-    );
-  } catch (error) {
-    console.error("Error loading page data:", error);
-    redirect("/error");
+  if (respondent?.is_finished_info) {
+    redirect("/");
     return null;
   }
+
+  return (
+    <Contents
+      sexItemList={sexItemList}
+      audioDeviceItemList={audioDeviceItemList}
+      respondent={respondent!}
+    />
+  );
 }
